c4fd16b0dfd9206550ec72e795294a70babca090
3onyc <3onyc@x3tech.com>
2013-12-21 00:24:23 +0100

    Add unit tests for Functional.php and placeholders for others

---
+++ Tests/FunctionalTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace x3\Functional\Tests;
+
+use x3\Functional\Functional as F;
+
+class FunctionalTest extends \PHPUnit_Framework_TestCase
+{
+    public function testReverseArgs()
+    {
+        $reversed = F::reverseArgs(function ($a, $b) {
+            return func_get_args();
+        });
+
+        $this->assertEquals(array(2, 1), $reversed(1, 2), 'Arguments not reversed');
+    }
+
+    public function testCurry()
+    {
+        $curried = F::curry(function ($a, $b) {
+            return func_get_args();
+        }, 1);
+
+        $this->assertEquals(array(1, 2), $curried(2), 'Method not curried');
+    }
+
+    /**
+     * Test the compose method, and verify that it runs in the correct order
+     */
+    public function testCompose()
+    {
+        $composed = F::compose(function ($a) {
+            return $a*2;
+        }, function ($a) {
+            return $a+1;
+        });
+
+        $this->assertEquals(4, $composed(1));
+    }
+
+    /**
+     * TODO Various datatypes
+     */
+    public function testMemoize()
+    {
+        $memoized = F::memoize(function ($min, $max) {
+            return rand($min, $max);
+        });
+
+        $this->assertEquals($memoized(1, 100), $memoized(1, 100));
+        $this->assertEquals($memoized(50, 70), $memoized(50, 70));
+    }
+}
