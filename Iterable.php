87154220c2dfb9f90623d7adc12861386d2e79df
3onyc <3onyc@x3tech.com>
2013-12-20 23:56:39 +0100

    Split methods into separate files

---
+++ Iterable.php
@@ -0,0 +1,167 @@
+<?php
+namespace x3\Functional;
+
+use Map;
+
+class Iterable
+{
+    /**
+     * array_map but for iterables instead of just arrays
+     *
+     * @param Iterable $iter     The iterable
+     * @param callable $callback The mapping callback
+     *
+     * @return array Mapped results
+     */
+    public static function map($iter, $callback)
+    {
+        return static::reduce($iter, function($result, $item, $key) use ($callback) {
+            $result[] = call_user_func($callback, $item, $key);
+            return $result;
+        }, array());
+    }
+
+    /**
+     * array_walk but for iterables
+     *
+     * @param Iterable $iter     The iterable
+     * @param callable $callback The callback to execute for each item
+     *
+     * @return null
+     */
+    public static function walk($iter, $callback)
+    {
+        foreach($iter as $item) {
+            $callback($item);
+        }
+    }
+
+    /**
+     * Similar to imap but also map the keys
+     *
+     * @param callable $callback  The callback to use
+     * @param array    $data      Data to map
+     * @param bool     $multiDict Whether to allow multiple values per key
+     *
+     * @return array The mapped result
+     */
+    public static function mapKeys($iter, $callback, $multiDict = false)
+    {
+        return static::pairsToDict(static::map($iter, $callback), $multiDict);
+    }
+
+    /**
+     * Group items of `$iter` by the result of `$keycallback`
+     *
+     * @param Iterable $iter        The iterable to parse
+     * @param callback $keyCallback A callback that returns the key to use
+     *
+     * @return array The grouped items
+     */
+    public static function groupBy($iter, $keyCallback)
+    {
+        $callback = function($item) use ($keyCallback) {
+            return array($keyCallback($item), $item);
+        };
+
+        return static::mapKeys($iter, $callback, true);
+    }
+
+    /**
+     * Find the first item that callback returns true for
+     *
+     * @param Iterable $iter     The iterable
+     * @param callable $callback The callback for checking
+     *
+     * @return bool|mixed The found item or false if none matched
+     */
+    public static function findIf($iter, $callback)
+    {
+        foreach ($iter as $item) {
+            if ($callback($item)) {
+                return $item;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * array_reduce but for iterables
+     *
+     * @param Iterable $iter     The iterable
+     * @param callable $callback The callback for the reduce
+     * @param mixed    $initial  Initial value for the reduce operation
+     *
+     * @return mixed The result of the reduce operation
+     */
+    public static function reduce($iter, $callback, $initial = null)
+    {
+        $result = $initial;
+        foreach ($iter as $key => $item) {
+            $result = $callback($result, $item, $key);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Convert key-value pairs to a key=>value array (dict)
+     *
+     * @param iterable $pairs The pairs to convert
+     * @param bool     $multiDict Wether to allow multiple values per key
+     *
+     * @return array key=>value dict
+     */
+    public static function pairsToDict($pairs, $multiDict = false)
+    {
+        $callback = function($result, $pair) use ($multiDict) {
+            list($key, $value) = $pair;
+            if($multiDict) {
+                if(!isset($result[$key])) {
+                    $result[$key] = array();
+                }
+                $result[$key][] = $value;
+            } else {
+                $result[$key] = $value;
+            }
+
+            return $result;
+        };
+
+        return static::reduce($pairs, $callback, array());
+    }
+
+    /**
+     * Pluck `$column` from the children of `$array`
+     *
+     * @param array $array  The array to pluck from
+     * @param mixed $column The column to return
+     *
+     * @return array The plucked columns
+     */
+    public static function pluck($array, $column)
+    {
+        return static::map(Map::key($column), $array);
+    }
+
+    /**
+     * Returns a closure that groups values by array key
+     *
+     * @param mixed $key, ... Keys to pluck and group by
+     *
+     * return callable Closure that accepts an iterable to group values from
+     */
+    public static function groupValues()
+    {
+        $keys = func_get_args();
+
+        return function($subjects) use ($keys) {
+            $callback = function($key) use ($subjects) {
+                return array($key, static::pluck($subjects, $key));
+            };
+
+            return static::mapKeys($callback, $keys);
+        };
+    }
+}
---
+++ Map.php
@@ -0,0 +1,49 @@
+<?php
+class Map
+{
+    /**
+     * Return a closure that calls `$method` on the object passed to it
+     * Mostly used for common *map operations
+     *
+     * @param string $method The method to call
+     *
+     * @return closure Calls `$method` on passed object
+     */
+    public static function method($method)
+    {
+        $args = array_slice(func_get_args(), 1);
+        return function($object) use($method, $args) {
+            return call_user_func_array(array($object, $method), $args);
+        };
+    }
+
+    /**
+     * Return a closure that returns `$attribute` of the object passed to it
+     * Mostly used for common *map operations
+     *
+     * @param string $attribute The attribute to reutrn
+     *
+     * @return closure Returns `$attribute` from passed object
+     */
+    public static function attribute($attribute)
+    {
+        return function($object) use($attribute) {
+            return $object->$attribute;
+        };
+    }
+
+    /**
+     * Return a closure that returns the value of `$key` for the passed array
+     * Mostly used for common *map operations
+     *
+     * @param string $key The key
+     *
+     * @return closure Returns value of `$key` on passed array
+     */
+    public static function key($key)
+    {
+        return function($array) use ($key) {
+            return $array[$key];
+        };
+    }
+}
